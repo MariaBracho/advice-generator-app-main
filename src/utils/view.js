125c67c1ccf92302046dda5cca56adc4546fb856
MariaBracho <mariabrachovillanueva@gmail.com>
2022-03-28 19:41:23 -0400

    improving code

--- src/utils/view.js
+++ src/utils/view.js
@@ -22,13 +22,13 @@ const render = () => {
      
      <p class="text-Light_Cyan my-auto text-2xl flex justify-center items-center h-60 "id="quotes"></p>
 
-     <img class="mb-7 mx-auto " src="${isMobile? mobile:desktop}">
+     <img alt="divider" class="mb-7 mx-auto " src="${isMobile? mobile:desktop}">
 
 
      </div>
 
      <div class="w-full absolute -bottom-6 mx-auto  ">
-     <button class="h-16 w-16  bg-Neon_Green rounded-full border-none outline-none cursor-pointer hover:drop-shadow-3xl " id="button"><img class="mx-auto" src="${iconDice}"></button>
+     <button class="h-16 w-16  bg-Neon_Green rounded-full border-none outline-none cursor-pointer hover:drop-shadow-3xl " id="button"><img alt="icon" class="mx-auto" src="${iconDice}"></button>
      </div>
      
 
